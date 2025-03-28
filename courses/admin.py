14ccc477196d681af76200ffbf4afd078e1961e4
Steve <stepan.capsamun@isa.utm.md>
2025-03-28 18:57:22 +0200

    added Task model

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Topic
+from .models import Course, Topic, Task
 
 
 @admin.register(Course)
@@ -16,3 +16,11 @@ class TopicAdmin(admin.ModelAdmin):
     list_filter = ("course",)
     search_fields = ("title", "explanation")
     ordering = ("course", "order")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("topic", "type", "question", "sentence_without_a_word", "variants", "missed_word",
+                    "sentence_for_ordering", "mp3", "text_to_speech", "order")
+    list_filter = ("topic", "type")
+
--- courses/models.py
+++ courses/models.py
@@ -33,3 +33,33 @@ class Topic(models.Model):
 
     def __str__(self):
         return f"{self.course.title} - {self.title}"
+
+
+class Task(models.Model):
+    type_choices = [
+        ("1", "Missed Word"),
+        ("2", "Ordering"),
+        ("3", "Pronunciation"),
+        ("4", "Audition")
+    ]
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="tasks")
+    type = models.CharField(max_length=1, choices=type_choices)
+    question = models.TextField()
+    sentence_without_a_word = models.TextField(blank=True)
+    variants = models.JSONField(default=list, blank=True)
+    missed_word = models.TextField(blank=True)
+    sentence_for_ordering = models.TextField(blank=True)
+    mp3 = models.FileField(upload_to="mp3/", blank=True)
+    text_to_speech = models.TextField(blank=True)
+    order = models.PositiveIntegerField(blank=True, default=1)
+
+    class Meta:
+        ordering = ["order"]
+
+    def save(self, *args, **kwargs):
+        if len(self.variants) != 4:  # Проверяем, что 4 элемента
+            raise ValueError("Variants must have exactly 4 elements")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.topic} - {self.order}"
